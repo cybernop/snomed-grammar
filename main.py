65e32848d027797419acd21cabe16049f0ef581d
Alexander Schulze <cibernop@gmail.com>
2020-04-15 16:29:14 +0200

    corrected grammar

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ spec = pathlib.Path('grammar.txt').read_text()
 spec = spec.splitlines()
 
 grammar = nltk.CFG.fromstring("""
-rule -> symbol SP ' = ' SP def
+S -> label SP '=' SP def
 SP -> ' '
-def -> symbol | string | ascii | OR | opt | comment | group | def SP def
+def -> label | string | ascii | OR | opt | comment | group
 group -> '(' def ')' | multi '(' def ')'
 comment -> def SP ';' SP name
 opt -> '[' def ']'
 OR -> def SP '/' SP def
 multi -> '*' | number | number '*' number | number '*'
-symbol -> name
+label -> name
 string -> '"' name '"'
 ascii -> '%' 'x' hex hex | '%' 'x' hex hex '-' hex hex
 number -> '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | number number
@@ -27,25 +27,10 @@ char -> 'A' | 'a' | 'B' | 'b' | 'C' | 'c' | 'D' | 'd' | 'E' | 'e' | 'F' | 'f' | 
 
 parser = nltk.ChartParser(grammar)
 
-
-entries = [
-    # 'equivalentTo = "==="',
-    # 'subtypeOf = "<<<"',
-    'foo = a',
-    'conceptId = sctId',
-    'definitionStatus = equivalentTo / subtypeOf',
-    # 'expression =  ws [definitionStatus ws] subExpression ws'
-]
-
-# for entry in entries:
-#     for tree in parser.parse(entry):
-#         tree.pretty_print()
-
 trees = []
 for i, entry in enumerate(spec):
     try:
         for tree in parser.parse(entry):
-            tree.pretty_print()
             trees.append(tree)
     except ValueError as e:
         print(f'{i}: {entry}: {e}')
