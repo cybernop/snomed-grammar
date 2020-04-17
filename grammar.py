a6fe5b04e86e2e647244d3c47bd5d8d0022ed5cb
Alexander Schulze <cibernop@gmail.com>
2020-04-17 08:07:30 +0200

    convert parsed spec into dict

--- grammar.py
+++ grammar.py
@@ -14,10 +14,56 @@ class GrammarBuilder:
     def parse(self, spec):
         for entry in spec:
             try:
-                for tree in self.parser.parse(entry):
-                    self.trees.append(tree)
+                trees = [tree for tree in self.parser.parse(entry)]
             except ValueError as e:
                 logger.error(f'{entry}: {e}')
+                trees = None
+            else:
+                if len(trees) == 0:
+                    logger.error(f'did not provide rule: {entry}')
+                else:
+                    self.trees += trees
 
-    def __dict__(self):
-        pass
+    def to_dict(self):
+        res = []
+        for tree in self.trees:
+            res.append(GrammarBuilder._tree_to_dict(tree))
+        return res
+
+    @staticmethod
+    def _tree_to_dict(tree):
+        DEF = 'def'
+
+        spec = []
+        for entry in tree:
+            if isinstance(entry, nltk.Tree):
+                label = entry.label()
+                if label == 'name':
+                    spec.append((label, ''.join(entry.leaves())))
+                elif label == 'string':
+                    spec.append((label, ''.join(entry.leaves())))
+                elif label == 'ascii':
+                    spec.append((label, ''.join(entry.leaves())))
+                elif label == 'SP':
+                    continue
+                else:
+                    spec.append((label, GrammarBuilder._tree_to_dict(entry)))
+            else:
+                spec.append(entry)
+
+        for i in range(0, len(spec)):
+            try:
+                key, value = spec[i]
+            except:
+                continue
+            else:
+                if key == DEF and isinstance(value, tuple):
+                    spec[i] = value
+
+        if len(spec) == 1:
+            spec = spec[0]
+
+        if spec[0] == 'name':
+            spec = spec[1]
+
+        return spec
--- main.py
+++ main.py
@@ -9,4 +9,6 @@ import grammar
 grammar_builder = grammar.GrammarBuilder(snomed.SPECIFICATION_GRAMMAR)
 grammar_builder.parse(snomed.read_spec('grammar.txt'))
 
+dict_ = grammar_builder.to_dict()
+
 print('foo')
